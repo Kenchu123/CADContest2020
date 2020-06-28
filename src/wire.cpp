e766cb17a83396146d94d3966f0a50a33a99cbe2
fanyungwei <brian.155406@gmail.com>
2020-06-28 22:22:35 +0800

    overload bus []

--- src/wire.cpp
+++ src/wire.cpp
@@ -2,6 +2,22 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    cout << "hi" << endl;
+short &
+Bus::operator[](int i)
+{   
+    if(i < 0 || i >= size){
+        cout << "\nError: Index " << i << " out of range" << endl;
+        exit(1);
+    }
+    return wires[i] -> val;
+}
+
+const short &
+Bus::operator[](int i) const
+{
+    if(i < 0 || i >= size){
+        cout << "\nError: Index " << i << " out of range" << endl;
+        exit(1);
+    }
+    return wires[i] -> val;
 }
