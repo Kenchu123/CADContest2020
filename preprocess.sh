931d5fd3c13cefcd55e6b96a980038e44068f429
fanyungwei <brian.155406@gmail.com>
2020-06-29 01:47:06 +0800

    add mkdir

--- preprocess.sh
+++ preprocess.sh
@@ -1,1 +1,2 @@
+mkdir ./small_circuit/intermediate
 ./bin/GraphPreprocessing.exe ./small_circuit/small.gv ./small_circuit/small.sdf /ICCAD2020_Benchmarks/GENERIC_STD_CELL.vlib ./small_circuit/intermediate
